696d1fd2dc44e7b6ff0fc3a1757b4f0e747612bd
ayoubdahdouh <ayoub.dahdouh.europe@gmail.com>
2021-08-29 18:34:29 +0200

    fixbug in ./lf -t /

--- src/lf.c
+++ src/lf.c
@@ -40,7 +40,7 @@ void run(linklist a)
         nm = (char *)it->data;
         if (!lf_stat(nm, &s))
         {
-            printf("%s: \"%s\" %s\n", PROGRAM, nm, strerror(errno));
+            printf("%s: \"%s\": %s\n", PROGRAM, nm, strerror(errno));
         }
         else
         {
@@ -57,7 +57,7 @@ void run(linklist a)
                     {
                         tree.parent_has_next = (char *)lf_alloc(sizeof(char) * PATH_MAX);
                     }
-                    if (LFpath[LFpathsiz - 1] == '/')
+                    if (LFpathsiz != 1 && LFpath[LFpathsiz - 1] == '/')
                     {
                         LFpathsiz--;
                         LFpath[LFpathsiz] = 0;
@@ -65,7 +65,7 @@ void run(linklist a)
                     display(LFpath, &s.st_mode, true);
                 }
                 // add slash if doesn't have it.
-                if (LFpath[LFpathsiz - 1] != '/')
+                if (LFpathsiz != 1 && LFpath[LFpathsiz - 1] != '/')
                 {
                     LFpath[LFpathsiz] = '/';
                     LFpathsiz++;
